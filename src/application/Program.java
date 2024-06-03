67737c5e88ecf47707bf31095ae5e89c48f7a94d
João Vitor Seiji Sato Calvaro <joaovssc@gmail.com>
2024-06-03 17:38:58 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package application;
 
-/**
- *
- * @author joaov
- */
+import boardgame.Position;
+
 public class Program {
-    
+    public static void main(String[] args){
+       Position pos = new Position(3,5);
+        System.out.println(pos);
+    }
 }
